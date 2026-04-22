8d1e4388afa91023aaa70edf190c1e2fdbfe3967
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-22 18:30:34 -0500

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        int a = 89;
+        int b = 3;
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+        }
+        }
