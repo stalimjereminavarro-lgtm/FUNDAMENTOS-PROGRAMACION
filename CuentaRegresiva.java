97b6a1018436f686ded594c324c187f708ad8898
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-22 19:16:34 -0500

    Add files via upload

---
+++ CuentaRegresiva.java
@@ -0,0 +1,10 @@
+public class CuentaRegresiva {
+    public static void main(String[] args) {
+        System.out.println("Cuenta regresiva:");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+            }
+            System.out.println("¡Despegue!");
+            }
+            }
+            
