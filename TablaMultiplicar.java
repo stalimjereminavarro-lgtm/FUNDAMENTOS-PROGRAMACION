49fdb932f97bcfee70aa30cae0b5db29c3f09e30
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-22 19:00:36 -0500

    Add files via upload

---
+++ TablaMultiplicar.java
@@ -0,0 +1,9 @@
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println("Tabla del " + numero);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+            }
+            }
+            }
