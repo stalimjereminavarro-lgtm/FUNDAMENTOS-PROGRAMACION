1f5af43b81e4e79f6796da6c2a788776697839b2
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-22 19:09:21 -0500

    Add files via upload

---
+++ SumaAcumulada.java
@@ -0,0 +1,11 @@
+public class SumaAcumulada {
+    public static void main(String[] args) {
+        int suma = 0;
+        int i = 1;
+        while (i <= 100) {
+            suma += i;
+            i++;
+            }
+            System.out.println("La suma del 1 al 100 es: " + suma);
+            }
+            }
