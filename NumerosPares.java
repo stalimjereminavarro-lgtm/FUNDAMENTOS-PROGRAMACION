24f74d81532b2d1a38aec101d7d27c1f890aea17
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-23 15:05:15 -0500

    Add files via upload

---
+++ NumerosPares.java
@@ -0,0 +1,11 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        System.out.println("Números pares del 1 al 20:");
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 != 0) {
+                continue;
+                }
+                System.out.print(i + " ");
+                }
+                }
+                }
