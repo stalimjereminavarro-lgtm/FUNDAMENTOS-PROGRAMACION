ffeedc5fbf8f963095dd19098e384f739a2ca241
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-27 16:14:28 -0500

    Add files via upload

---
+++ SumaNumeros3.java
@@ -0,0 +1,11 @@
+public class SumaNumeros3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int suma = a + b + c;
+
+        System.out.println("La suma de " + a + " + " + b + " + " + c + " = " + suma);
+    }
+}
