124ebfce11dda89deedbde05c1182e18817dec71
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-04-27 16:18:44 -0500

    Add files via upload

---
+++ ParImpar2.java
@@ -0,0 +1,10 @@
+public class ParImpar2 {
+    public static void main(String[] args) {
+        int numero = 24;
+     if (numero % 2 == 0) {
+        System.out.println(numero + " es par");
+        } else {
+            System.out.println(numero + " es impar");
+            }
+            }
+            }
