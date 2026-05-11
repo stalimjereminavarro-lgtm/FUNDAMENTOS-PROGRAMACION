23ea799c861e3dfd17c6a0e785888bc24cc65571
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-05-11 09:44:47 -0500

    Add files via upload

---
+++ Concatenacion.java
@@ -0,0 +1,11 @@
+class Concatenacion {
+    public static void main(String[] args) {
+
+        String nombre = "John";
+        String primerApellido = "Doe";
+
+        String nombreCompleto = nombre + " " + primerApellido;
+
+        System.out.println(nombreCompleto);
+    }
+}
