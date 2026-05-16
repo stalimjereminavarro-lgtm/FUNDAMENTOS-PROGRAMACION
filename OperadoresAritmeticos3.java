544e2c4accbe129ae24cb39de6eb4e8d687c4880
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-05-16 12:35:04 -0500

    Add files via upload

---
+++ OperadoresAritmeticos3.java
@@ -0,0 +1,18 @@
+class OperadoresAritmeticos3 {
+
+    public static void main(String[] args) {
+
+        double numero1 = 7.0;
+        double numero2 = 5.0;
+
+        System.out.println("Operaciones con \"numero1\" y \"numero2\":");
+
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
+        System.out.println(numero1 % numero2);
+
+    } // Fin del método main
+
+} // Fin de la clase OperadoresAritmeticos
