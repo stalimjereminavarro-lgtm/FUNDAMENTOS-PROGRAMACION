62d6eb4507bec043e2feb7dc2b46e40ef502e99e
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-05-16 11:51:36 -0500

    Add files via upload

--- Concatenacion.java
+++ Concatenacion.java
@@ -1,11 +1,15 @@
 class Concatenacion {
+
     public static void main(String[] args) {
 
         String nombre = "John";
         String primerApellido = "Doe";
 
-        String nombreCompleto = nombre + " " + primerApellido;
+        String nombreConApellido =
+                nombre + " " + primerApellido;
 
-        System.out.println(nombreCompleto);
-    }
-}
+        System.out.println(nombreConApellido);
+
+    } // Fin del método main
+
+} // Fin de la clase Concatenacion
