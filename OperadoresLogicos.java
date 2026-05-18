5e74b15de659acde3931d78866019f338cd53d46
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-05-18 08:26:12 -0500

    Add files via upload

---
+++ OperadoresLogicos.java
@@ -0,0 +1,27 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== OPERADOR AND (&&) =====");
+        System.out.println("true && true = " + (true && true));
+        System.out.println("true && false = " + (true && false));
+        System.out.println("false && true = " + (false && true));
+        System.out.println("false && false = " + (false && false));
+
+        System.out.println();
+
+        System.out.println("===== OPERADOR OR (||) =====");
+        System.out.println("true || true = " + (true || true));
+        System.out.println("true || false = " + (true || false));
+        System.out.println("false || true = " + (false || true));
+        System.out.println("false || false = " + (false || false));
+
+        System.out.println();
+
+        System.out.println("===== OPERADOR NOT (!) =====");
+        System.out.println("!true = " + (!true));
+        System.out.println("!false = " + (!false));
+
+    }
+
+}
