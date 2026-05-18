ab341ccb722b4f97b457ce42664fe34f47111c17
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-05-18 09:20:10 -0500

    Add files via upload

---
+++ InstruccionIf.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class InstruccionIf {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("===== VERIFICAR MAYOR DE EDAD =====");
+
+        System.out.print("Ingrese una edad: ");
+        int edad = entrada.nextInt();
+
+        if (edad >= 18) {
+            System.out.println("La persona es mayor de edad.");
+        }
+
+            System.out.println("Adiós!");
+        entrada.close();
+    }
+
+}
