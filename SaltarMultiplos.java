5074030718e07fe6446e88bf1af62ac4b6e5d63a
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-01 10:27:59 -0500

    Add files via upload

---
+++ SaltarMultiplos.java
@@ -0,0 +1,13 @@
+public class SaltarMultiplos {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+
+            if (i % 5 == 0) {
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+    }
+}
