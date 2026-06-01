902fdcb46a8511309c9a47b3a3eba7a7d287aa0d
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-01 09:45:00 -0500

    Add files via upload

--- TablaMultiplicar.java
+++ TablaMultiplicar.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class TablaMultiplicar {
     public static void main(String[] args) {
-        int numero = 5;
-        System.out.println("Tabla del " + numero);
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(numero + " x " + i + " = " + (numero * i));
-            }
-            }
-            }
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
