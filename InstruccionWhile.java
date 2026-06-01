30a09a1214ea49023a019b180cb0a0b7c361c048
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-01 15:52:08 -0500

    Add files via upload

---
+++ InstruccionWhile.java
@@ -0,0 +1,14 @@
+public class InstruccionWhile {
+
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while (numero <= 10) {
+            System.out.println("Texto " + numero);
+            numero = numero + 1;
+        }
+
+    } // Fin del método main
+
+} // Fin de la clase InstruccionWhile
