7f2224b70a6d7056d9eaf4b7d8b7d3e020894b92
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-01 15:48:17 -0500

    Add files via upload

---
+++ ImprimiendoTextos.java
@@ -0,0 +1,18 @@
+public class ImprimiendoTextos {
+
+    public static void main(String[] args) {
+
+        System.out.println("Texto 1");
+        System.out.println("Texto 2");
+        System.out.println("Texto 3");
+        System.out.println("Texto 4");
+        System.out.println("Texto 5");
+        System.out.println("Texto 6");
+        System.out.println("Texto 7");
+        System.out.println("Texto 8");
+        System.out.println("Texto 9");
+        System.out.println("Texto 10");
+
+    } // Fin del método main
+
+} // Fin de la clase ImprimiendoTextos
