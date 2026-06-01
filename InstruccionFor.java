344a71db3a7a6bc2dfc695f10a33d95d0d053ef5
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-01 15:55:42 -0500

    Add files via upload

---
+++ InstruccionFor.java
@@ -0,0 +1,11 @@
+public class InstruccionFor {
+
+    public static void main(String[] args) {
+
+        for (int numero = 1; numero <= 10; numero = numero + 1) {
+            System.out.println("Texto " + numero);
+        }
+
+    } // Fin del método main
+
+} // Fin de la clase InstruccionFor
