594c25de724cfadf574d99fa12ce1b9bd3d774d3
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-22 09:13:37 -0500

    Add files via upload

---
+++ Numeros1al50.java
@@ -0,0 +1,10 @@
+public class Numeros1al50 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            if (i == 30) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
