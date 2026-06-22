5c4285391195bb113328f054f2c327922e63ff7d
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-06-22 09:16:14 -0500

    Add files via upload

---
+++ Numeros1al100c.java
@@ -0,0 +1,10 @@
+public class Numeros1al100c {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
