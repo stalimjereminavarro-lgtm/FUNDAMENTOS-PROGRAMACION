160bc8fc0cfc032d760a826fd251b601f4e3d809
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-07-13 09:25:22 -0500

    Add files via upload

---
+++ ForMejorado.java
@@ -0,0 +1,14 @@
+class ForMejorado {
+
+    public static void main(String[] args) {
+
+        String[] dias = {"LUN", "MAR", "MIE",
+                         "JUE", "VIE", "SAB", "DOM"};
+
+        for (String dia : dias) {
+            System.out.println(dia);
+        }
+
+    } // Fin del método main
+
+} // Fin de la clase ForMejorado
