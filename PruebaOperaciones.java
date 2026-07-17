a330b51591f522fb084e14fc4755b74d0abbd67d
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-07-17 17:16:26 -0500

    Add files via upload

--- PruebaOperaciones.java
+++ PruebaOperaciones.java
@@ -1,13 +1,11 @@
+package c05.p04;
+
 class PruebaOperaciones {
-
     public static void main(String[] args) {
-
         Operaciones operaciones = new Operaciones();
 
         operaciones.imprimirTexto("Primer Texto");
         operaciones.imprimirTexto("Segundo Texto");
         operaciones.imprimirTexto("Tercer Texto");
-
     } // Fin del método main
-
-} // Fin de la clase PruebaOperaciones
+} // Fin de la Clase PruebaOperaciones
