d5bae0dbf91ec8e2c37cb5d06bf58047a84559f4
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-07-17 17:43:41 -0500

    Add files via upload

--- PruebaOperaciones.java
+++ PruebaOperaciones.java
@@ -1,14 +1,15 @@
-package c05.p05;
+package c05.p06;
 
 class PruebaOperaciones {
     public static void main(String[] args) {
         Operaciones operaciones = new Operaciones();
 
-        operaciones.concatenarTexto("Primero", "Segundo");
-        operaciones.concatenarTexto("Tercero", "Cuarto");
+        int resultado = operaciones.sumar(10, 20);
+        System.out.println( resultado );
 
-        operaciones.imprimirSuma(10, 20);
-        operaciones.imprimirSuma(30, 40);
+        System.out.println( operaciones.restar(30, 9) );
+
+        System.out.println( operaciones.cuadrado(11) );
 
     } // Fin del método main
 } // Fin de la Clase PruebaOperaciones
