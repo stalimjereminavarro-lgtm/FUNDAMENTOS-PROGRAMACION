6ee17d72be28026831540f5bb0f3bfa341a6feae
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-07-16 20:20:37 -0500

    Add files via upload

--- package c05.java
+++ package c05.java
@@ -1,9 +1,33 @@
 package c05.p01;
 
-class Persona {
-    String nombre;
-    String apellidoPaterno;
-    String apellidoMaterno;
-    int edad;
-    int peso;
-} // Fin de la clase Persona
+class PruebaPersona {
+
+    public static void main(String[] args) {
+
+        Persona persona1 = new Persona();
+
+        // Se imprimen los valores iniciales del objeto "persona1"
+        System.out.println("Nombre: " + persona1.nombre);
+        System.out.println("Ap. Paterno: " + persona1.apellidoPaterno);
+        System.out.println("Ap. Materno: " + persona1.apellidoMaterno);
+        System.out.println("Edad: " + persona1.edad);
+        System.out.println("Peso: " + persona1.peso);
+
+        // Se asignan valores a los campos del objeto "persona1"
+        persona1.nombre = "Alex";
+        persona1.apellidoPaterno = "Torres";
+        persona1.apellidoMaterno = "Flores";
+        persona1.edad = 20;
+        persona1.peso = 56;
+
+        // Se imprimen los valores del objeto "persona1"
+        System.out.println();
+        System.out.println("Nombre: " + persona1.nombre);
+        System.out.println("Ap. Paterno: " + persona1.apellidoPaterno);
+        System.out.println("Ap. Materno: " + persona1.apellidoMaterno);
+        System.out.println("Edad: " + persona1.edad);
+        System.out.println("Peso: " + persona1.peso);
+
+    } // Fin del método main
+
+} // Fin de la clase PruebaPersona
