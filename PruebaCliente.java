ef477b97fa893b6780876403208ae56ae6a29fb3
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-07-17 19:04:09 -0500

    Add files via upload

---
+++ PruebaCliente.java
@@ -0,0 +1,15 @@
+package c07.p01;
+
+public class PruebaCliente {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente();
+
+        cliente1.setNombre("Alberto");
+        cliente1.setApellidoPaterno("Arenas");
+        cliente1.setApellidoMaterno("Aguirre");
+        cliente1.setNumeroDeCliente(1001);
+
+        cliente1.imprimirCampos();
+
+    } // Fin del método main
+} // Fin de la clase PruebaCliente
