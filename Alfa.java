b849129111693f54a0c11b1380bdcaff79c8d2f2
stalimjereminavarro-lgtm <stalimjereminavarro@gmail.com>
2026-07-17 20:35:14 -0500

    Add files via upload

---
+++ Alfa.java
@@ -0,0 +1,17 @@
+package c07.p03.uno;
+
+public class Alfa {
+
+    public int campoPublic;
+    int campoDefault;
+    protected int campoProtected;
+    private int campoPrivate;
+
+    public Alfa() {
+        campoPublic = 0;
+        campoDefault = 0;
+        campoProtected = 0;
+        campoPrivate = 0;
+    } // Fin del constructor Alfa
+
+} // Fin de la clase Alfa
